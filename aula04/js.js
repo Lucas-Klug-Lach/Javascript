553fd2360cfb03ee650485cf9be2be6b29bfa1c3
Lucas Klug Lach <167248764+Lucas-Klug-Lach@users.noreply.github.com>
2025-01-10 18:54:48 -0300

    Update Javascript

--- aula04/js.js
+++ aula04/js.js
@@ -1,3 +1,3 @@
-window.alert('Minha primeira mensagem') //Manda um alerta
+window.alert('Minha primeira mensagem')  //Manda um alerta
 window.confirm('Está gostando de Js?')  //Manda uma tela de confirmação
 window.prompt('Qual é o seu nome?') //Manda um prompt para colocar o nome do usuário
