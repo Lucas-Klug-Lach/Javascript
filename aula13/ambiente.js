d0f52b4696261ac998c31edc049b4addbf6adb6c
Lucas Klug Lach <167248764+Lucas-Klug-Lach@users.noreply.github.com>
2025-01-16 16:06:38 -0300

    update aula14

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+var c = 0
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 10000)
+
+
+
+
+
+/*
+var cont = 1
+while (cont <= 1000) {
+    console.log(`Passo ${cont}`)
+    cont++
+}*/
+
+
+
+
+
+
+
+
+//console.log('Tudo bem?')
